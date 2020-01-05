650825a2a14173840303cffd78492182f8854d3f
Jake <jakeshin22@gmail.com>
2020-01-04 22:18:03 -0500

    Reorganized components and started additional information on player expanded info

--- src/components/playerExpandedInfo/playerExpandedInfo.jsx
+++ src/components/playerExpandedInfo/playerExpandedInfo.jsx
@@ -1,37 +1,57 @@
 import React from "react";
 
 function PlayerInfo(props) {
-  function getPlayerName(puuid) {
-    for (var i = 0; i < props.challengerData.length; i++) {
-      if (props.challengerData[i].PUUID === puuid) {
-        return props.challengerData[i].Summoner_Name;
-      }
-    }
-  }
-
-  function getPlayerHistory(puuid) {
-    return props.matchData.filter(function(playerInfo) {
-      if (playerInfo.PUUID === puuid) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
-
   return (
     <div>
       <div>
-        {getPlayerHistory(props.puuid).map(({ PUUID, Match_ID }, idx) => (
-          <div key={idx}>
-            <div>
-              <h4>{getPlayerName(PUUID)}</h4>
-              <h4>{Match_ID}</h4>
+        <h4>{getPlayerName(props)}</h4>
+      </div>
+      <div>
+        {getPlayerHistory(props).map(
+          ({ Match_ID, Placement, Champions, Traits }, idx) => (
+            <div key={idx}>
+              <div>
+                {/* <h4>{Match_ID}</h4> */}
+                <h4>Placement</h4>
+                <h4>{Placement}</h4>
+                <h4>Champions</h4>
+                <h4>{loopThroughChampions(Champions)}</h4>
+                {/* <h4>{Traits}</h4> */}
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
 }
+
+function getPlayerName(props) {
+  for (var i = 0; i < props.challengerData.length; i++) {
+    if (props.challengerData[i].PUUID === props.puuid) {
+      return props.challengerData[i].Summoner_Name;
+    }
+  }
+}
+
+function getPlayerHistory(props) {
+  return props.matchData.filter(function(playerInfo) {
+    if (playerInfo.PUUID === props.puuid) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+function loopThroughChampions(champlist) {
+  var asdf = "";
+
+  for (let champobj of champlist) {
+    asdf = asdf.concat(JSON.stringify(champobj));
+  }
+
+  return asdf;
+}
+
 export default PlayerInfo;
